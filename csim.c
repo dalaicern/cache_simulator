2222c30c2202516cedbf454fbe856274260b04b2
dalaicern <dalai.num@gmail.com>
2025-02-28 14:17:33 +0800

    struct added

--- csim.c
+++ csim.c
@@ -1,4 +1,40 @@
+#include <stdlib.h>
 #include "cachelab.h"
+#include <getopt.h>
+#include <stdio.h>
+#include <math.h>
+
+#define ll long long
+#define ull unsigned long long
+
+typedef char* string;
+
+struct Para{
+    int s, S;
+    int E;
+    int b, B;
+    int hits, misses, evicts, verbose;
+};
+
+struct Line{
+    int uses, valid;
+    ull tag;
+    string block;
+};
+
+struct Set{
+    struct Line* lines;
+};
+
+struct Cache{
+    struct Set* sets;
+};
+
+struct Cache cacheBuild(){
+    
+}
+
+
 
 int main()
 {
